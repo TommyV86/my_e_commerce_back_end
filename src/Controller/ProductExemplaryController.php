2c8cc32cfb6be9d9fe9fdb53327880385c3e3b7d
TommyV86 <tommychel@hotmail.fr>
2024-03-01 11:33:26 +0100

    persistance des produits en exemplaire ok, à réadapter pour le front

--- src/Controller/ProductExemplaryController.php
+++ src/Controller/ProductExemplaryController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
-use App\Service\EntityService\ProducExemplaryService;
+use App\Service\EntityService\ProductExemplaryService;
 use App\Service\EntityService\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+
+use App\Repository\CartRepository;
+
 
 
 class ProductExemplaryController extends AbstractController
@@ -17,14 +20,14 @@ class ProductExemplaryController extends AbstractController
 
     #[Route('/confirm_product_exemplary_in_cart', name: 'app_product_exemplary', methods: ['POST'])]
     public function index(
-        ProducExemplaryService $producExemplaryService,
-        CartService $cartService,
-        Request $request): JsonResponse
+        ProductExemplaryService $productExemplaryService,
+        Request $request,
+        CartRepository $cartRepository): JsonResponse
     {
         // récupération du panier persisté de l'user 
         // persistance des produits exemplaires contenu dans le panier du user dans la db
         
-        $this->isSaved = $producExemplaryService->save($request);
+        $this->isSaved = $productExemplaryService->save($request, $cartRepository);
         $this->message = $this->isSaved ? 'prod ex with cart are registered successfully!' : 'error in the server';
         
         return $this->json(['message' => $this->message]);
--- src/Service/EntityService/ProductExemplaryService.php
+++ src/Service/EntityService/ProductExemplaryService.php
@@ -2,12 +2,16 @@
 
 namespace App\Service\EntityService;
 
+use App\Entity\Cart;
 use App\Entity\ProductExemplary;
+use App\Repository\CartRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\SerializerInterface;
 use Doctrine\ORM\EntityManagerInterface;
 
-class ProducExemplaryService {
+class ProductExemplaryService 
+{
 
     private $serializer;
     private $entityManager;
@@ -20,15 +24,39 @@ class ProducExemplaryService {
         $this->entityManager = $entityManager;
     }
 
-    public function save(Request $request) : bool {
+    public function save(
+        Request $request, 
+        CartRepository $cartRepository) : bool {
+
+        //deserialisation lorsque le front sera developpé
+        //à réadapter pour le front, ajouter une vérification via le role
 
         $data = $request->getContent();
-        $prodEx = $this->serializer->deserialize($data, ProductExemplary::class, "json");
+        $datasIntoArray = json_decode($data, true);
 
-        $this->entityManager->persist($prodEx);
+        $idCart = (int) $datasIntoArray['id'];
+        $cart = $cartRepository->find($idCart);
+
+        $prodExs = new ArrayCollection();
+
+        $prodExs->add($datasIntoArray['productExemplary']);
+        $prodExs->add($datasIntoArray['productExemplary']);
+        $prodExs->add($datasIntoArray['productExemplary']);
+
+        //boucler dans la collection et persister chaque prod ex
+
+        foreach ($prodExs as $prodEx) {
+            
+            $prodEx = $this->serializer->deserialize($data, ProductExemplary::class, "json");
+            $prodEx->setCart($cart);
+
+            $this->entityManager->persist($prodEx);
+        }
+        
+        //$this->entityManager->persist();
         $this->entityManager->flush();
 
-        return true;
+        return $prodExs === null ? false : true;
     }
 
 }
