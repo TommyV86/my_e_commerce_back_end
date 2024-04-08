297430b4854701d9ada4e1f04d74f2780ab08930
TommyV86 <tommychel@hotmail.fr>
2024-04-08 10:19:17 +0200

    variables typés et rendus en attribut de classe

--- src/EventSubscriber/JWTAuthenticationSuccessSubscriber.php
+++ src/EventSubscriber/JWTAuthenticationSuccessSubscriber.php
@@ -2,17 +2,25 @@
 
 namespace App\EventSubscriber;
 
+use App\Entity\Dto\PersonDtos\PersonDto;
+use Doctrine\Common\Collections\Collection;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class JWTAuthenticationSuccessSubscriber implements EventSubscriberInterface
 {
 
-    private $userProvider;
-    private $jwtManager;
+    private UserProviderInterface $userProvider;
+    private JWTTokenManagerInterface $jwtManager;
+    private Response $response;
+    private array $data;
+    private string $jwt;
+    private Cookie $cookie;
+
 
     public function __construct(
         UserProviderInterface $userProvider, 
@@ -22,23 +30,23 @@ class JWTAuthenticationSuccessSubscriber implements EventSubscriberInterface
         $this->jwtManager = $jwtManager;
     }
 
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents() : array
     {
         return [
             'lexik_jwt_authentication.on_authentication_success' => 'onAuthenticationSuccess',
         ];
     }
 
-    public function onAuthenticationSuccess(AuthenticationSuccessEvent $event)
+    public function onAuthenticationSuccess(AuthenticationSuccessEvent $event) : void
     {
 
-        $response = $event->getResponse();
-        $data = $event->getData();
-        $jwt = $data['token']; 
+        $this->response = $event->getResponse();  
+        $this->data = $event->getData();
+        $this->jwt = $this->data['token']; 
 
         // Créer un cookie sécurisé HttpOnly qui expire dans 3600 secondes (1 heure)
-        $cookie = Cookie::create('BEARER')
-                        ->withValue($jwt)
+        $this->cookie = Cookie::create('BEARER')
+                        ->withValue($this->jwt)
                         ->withExpires(time() + 3600)
                         ->withPath('/')
                         ->withSecure(true) // true si vous utilisez HTTPS, false sinon
@@ -48,8 +56,8 @@ class JWTAuthenticationSuccessSubscriber implements EventSubscriberInterface
                         //Todo config à modifier pour le deploiement
 
         // Ajouter le cookie à l'objet Response
-        $response->headers->setCookie($cookie);
-        $event->setData($data);
+        $this->response->headers->setCookie($this->cookie);
+        $event->setData($this->data);
     }
 
     
